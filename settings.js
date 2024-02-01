854ca5f6ba3a67a987501745afa470c450972dd9
Andro Pieter Klemens <121905288+androklemens@users.noreply.github.com>
2024-02-01 15:15:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'AndroKlemens'// yourname
+global.MONGO_DB_URI = "database mongodb" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "ambonpunya69@gmail.com" //email
+global.email_password = "08881415520" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
